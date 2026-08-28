f3624ac654d0711047576e82ec19f4c52dfed04f
Bariya33 <manthanbariya015@gmail.com>
2026-08-28 23:03:51 +0530

    Add founders section and team images

--- src/data/content.js
+++ src/data/content.js
@@ -89,6 +89,9 @@ export const CATEGORIES = [
   },
 ];
 
+src=`${BASE_URL}media/images/team/manthan.jpeg`
+src=`${BASE_URL}media/images/team/ajay.jpeg`
+
 export const PROJECTS = [
   {
     id: 'cars-bikes-01',
